ea82d53f3a8cb50c123c5820c831cc819269b3d9
aq-codes <vpaqil3@gmail.com>
2024-08-11 17:11:10 +0530

    completed the website and made it responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// navigation bar icon toggle
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -10,29 +12,4 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
-const form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
-    e.preventDefault;
-    //do nothing if formnot validated
-    if(!validateForm(form)) return;
-});
 
-const validateForm = (form) => {
-    let valid = true;
-    //check for empty fields
-    let name = form.querySelector(".name");
-    let message = form.querySelector(".message");
-    let email = form.querySelector(".email")
-
-    if (name.value === ""){
-        giveError(name, "Please enter your name");
-
-    }
-}
-
-const giveError = (field, message) => {
-    let parentElement = field.parentElement;
-    parentElement.classList.add("error");
-    
-
-}
